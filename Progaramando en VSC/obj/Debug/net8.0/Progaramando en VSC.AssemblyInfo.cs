3315df303c292f9f2a69c260fca026b4ba86eb1d
BryQuis <bryan.quisbert.t@ucb.edu.bo>
2024-08-12 12:31:11 -0400

    12/08/24
    3 funciones en C#:
    Hola Mundo
    Ingresar Edad
    Obtener Edad

--- Progaramando en VSC/obj/Debug/net8.0/Progaramando en VSC.AssemblyInfo.cs
+++ Progaramando en VSC/obj/Debug/net8.0/Progaramando en VSC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Progaramando en VSC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1848dbb0ec243381790e9bde99d3f7e9d0ac894c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2442b91ee92773e66ca330bab6c1b4de30f016a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Progaramando en VSC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Progaramando en VSC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
