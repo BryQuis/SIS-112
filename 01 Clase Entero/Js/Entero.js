663e2a83837156814c3917e2774e7ba9bdc980e9
BryQuis <bryan.quisbert.t@ucb.edu.bo>
2024-09-13 10:48:39 -0400

    A

--- 01 Clase Entero/Js/Entero.js
+++ 01 Clase Entero/Js/Entero.js
@@ -1,16 +1,52 @@
-class Entero{
-    // atributos
+class Entero {
+    //Atributos
     Num;
-    
-    // constructor}
+    //Constructor = inicializa el objeto
     constructor(Numero){
         this.Num = Numero;
     }
-
-    //Obtener Valor 
-    ObtenerNumero(){
+    //CLASS - Cargar el valor de Num
+    setNum(){
+        const input = document.getElementById("numeroInput");
+        this.Num = parseInt(input.ariaValueMax, 10);
+    }
+    //CLASS - Obtener el valor de Num
+    getNum() {
         return this.Num;
     }
+    //CLASS - Mostrar el valor de Num
+    showNum(){
+        const resultadoDiv = document.getElementById('resultado');
+        resultadoDiv.textContent = this.getNum();
+    }
 
-    // Varios Metodos 
+    IncrementarNum(){
+        this.Num =this.Num + 1;
+    }
+    DecrementarNum(){
+        this.Num =this.Num - 1;
+    }
+}
+
+//Las funciones = button HTML
+var ClaseEntero = new Entero(0); //Se inicializo en 0
+
+//Crear el objeto
+function cargarNum() {
+    ClaseEntero.setNum();
+}
+
+function mostrarNum(){
+    ClaseEntero.showNum();
+}
+// Incrementar el valor de Num
+function IncrementarValor(){
+    ClaseEntero.IncrementarNum();
+    ClaseEntero.showNum();
+}
+
+//Decrementar el valor Num
+function DecrementarValor(){
+    ClaseEntero.DecrementarNum();
+    ClaseEntero.showNum();
 }
