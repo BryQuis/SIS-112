70858fea647a0398c7465ea2c106984f855958d9
BryQuis <bryan.quisbert.t@ucb.edu.bo>
2024-10-04 09:32:16 -0400

    nose

---
+++ POO/js/Persona.js
@@ -0,0 +1,52 @@
+class Persona {
+    constructor(nombre, edad, carrera, universidad) {
+      this.nombre = nombre;
+      this.edad = edad;
+      this.carrera = carrera;
+      this.universidad = universidad;
+    }
+  
+    saludar() {
+      return 'Hola, mi nombre es ' + this.nombre + ' y tengo ' + this.edad + ' años.'
+    }
+  
+    cumpleanios() {
+      return '¡Felicidades! tienes ' + this.edad + ' años.';
+    }
+  
+    estudiar() {
+      return 'Estoy estudiando ' + this.carrera;
+    }
+    Universidad() {
+        return 'Estoy en la universidad ' + this.universidad;
+      }
+  }
+  
+  // Crear una instancia de la clase Persona
+  const persona = new Persona('Bryan', 18, 'Ingeniería Industrial' , "Catolica Boliviana");
+  
+  // Inicializar los elementos HTML
+  const saludar = document.getElementById('saludar');
+  const edad = document.getElementById('edad');
+  const carrera = document.getElementById('carrera');
+  const universidad = document.getElementById('universidad');
+  
+  // Función para saludar
+  function botonSaludar() {
+    saludar.textContent = persona.saludar();
+  }
+  
+  // Función para cumpleaños
+  function botonCumpleanios() {
+    edad.textContent = persona.cumpleanios();
+  }
+  
+  // Función para estudiar
+  function botonEstudiar() {
+    carrera.textContent = persona.estudiar();
+  }
+  // Función para universidad
+  function botonUniversidad() {
+    carrera.textContent = persona.Universidad();
+  }
+
